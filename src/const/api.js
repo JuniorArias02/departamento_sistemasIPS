ec88a46c32b6a1b6d1535a373b515ed2d8c0438a
JuniorArias02 <junior.arias02yt@gmail.com>
2025-05-24 09:21:16 -0500

    conexion

--- src/const/api.js
+++ src/const/api.js
@@ -1,5 +1,6 @@
-const PUERTO = 8888;
-const DOMINIO = "localhost";
+// const PUERTO = 8888;
+// const DOMINIO = "localhost";
 
-export const URL_PATH = `http://${DOMINIO}:${PUERTO}/`;
+// export const URL_PATH = `http://${DOMINIO}:${PUERTO}/`;
+export const URL_PATH = 'https://psicologicamentehablando.space/formApi/';
 // export const URL_PATH = `https://identifier-shaped-warrant-arch.trycloudflare.com/`;
