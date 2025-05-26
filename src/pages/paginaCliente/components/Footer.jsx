6bfac1ad8dcc109114beebdbd7f557051059691b
JuniorArias02 <junior.arias02yt@gmail.com>
2025-05-26 12:27:19 -0500

    se implemente numero de version

--- src/pages/paginaCliente/components/Footer.jsx
+++ src/pages/paginaCliente/components/Footer.jsx
@@ -1,8 +1,13 @@
+import getVersion from "../../../../version";
 export default function Footer() {
+  const { version, releaseDate } = getVersion();
   return (
     <footer className="bg-gray-200 text-center py-4 mt-auto shadow-inner">
       <p className="text-gray-700 font-medium">
         IPS CLINICAL HOUSE - Departamento de sistemas 💻
+      </p>
+      <p className="text-gray-400 font-medium">
+        Versión: {version}  <br />  Actualizado - {releaseDate}
       </p>
     </footer>
   );
---
+++ version.jsx
@@ -0,0 +1,5 @@
+export default function getVersion() {
+  const version = "1.5";
+  const releaseDate = "26/05/2025";
+  return { version, releaseDate };
+}
