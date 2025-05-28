244fbfc8ec9adc7b8ae355d69eaf4b948d9ee7f2
JuniorArias02 <junior.arias02yt@gmail.com>
2025-05-28 16:37:53 -0500

    version

--- version.jsx
+++ version.jsx
@@ -1,5 +1,5 @@
 export default function getVersion() {
-  const version = "1.5.2";
-  const releaseDate = "26/05/2025";
+  const version = "1.5.3";
+  const releaseDate = "28/05/2025";
   return { version, releaseDate };
 }
