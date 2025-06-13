0bb67b5f65b0591f5d2588f3356d4dcc39fd8f0d
JuniorArias02 <junior.arias02yt@gmail.com>
2025-06-12 19:25:45 -0500

    nuevas modificaicones

--- src/const/api.js
+++ src/const/api.js
@@ -2,7 +2,7 @@
 // const DOMINIO = "localhost";
 
 // export const URL_PATH = `http://${DOMINIO}:${PUERTO}/`;
-// export const URL_PATH = 'https://psicologicamentehablando.space/formApi/';
-export const URL_PATH = 'http://localhost:8888/';
+export const URL_PATH = 'https://psicologicamentehablando.space/formApi/';
+// export const URL_PATH = 'http://localhost:8888/';
 // export const URL_PATH = 'https://profits-modified-cookie-abroad.trycloudflare.com/';
 
