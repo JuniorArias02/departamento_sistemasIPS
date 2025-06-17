19b45f7b8a096774d6a1dd3a68f8541ff23ef146
JuniorArias02 <junior.arias02yt@gmail.com>
2025-06-16 23:20:32 -0500

    correcion de mantenimiento

--- src/pages/paginaCliente/Dashboard.jsx
+++ src/pages/paginaCliente/Dashboard.jsx
@@ -26,7 +26,7 @@ export default function Dashboard() {
 
 				setTotales({
 					inventario: inventario,
-					mantenimientoFreezer: mantenimiento
+					mantenimiento: mantenimiento
 
 				});
 			} catch (error) {
