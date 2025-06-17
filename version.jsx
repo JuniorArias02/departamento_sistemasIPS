24b90bc25311499612cb191ee752d34f752f9aaa
Junior Arias <158210736+JuniorArias02@users.noreply.github.com>
2025-06-17 15:35:59 -0500

    Update version.jsx

--- version.jsx
+++ version.jsx
@@ -1,5 +1,5 @@
 export default function getVersion() {
-  const version = "2.1002 beta";
-  const releaseDate = "16/06/2025";
+  const version = "2.1003 beta";
+  const releaseDate = "17/06/2025";
   return { version, releaseDate };
 }
