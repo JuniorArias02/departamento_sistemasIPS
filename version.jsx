0085c2199e11dfadf63750625ed8f8438817188f
JuniorArias02 <junior.arias02yt@gmail.com>
2025-06-23 02:58:27 -0500

    correcion de version

--- version.jsx
+++ version.jsx
@@ -1,5 +1,5 @@
 export default function getVersion() {
   const version = "2.3 beta";
-  const releaseDate = "19/06/2025";
+  const releaseDate = "23/06/2025";
   return { version, releaseDate };
 }
