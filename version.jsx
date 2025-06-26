ea3b860275b0300df77acb3855ce3c13e20865bb
JuniorArias02 <junior.arias02yt@gmail.com>
2025-06-26 13:48:46 -0500

    debug

--- version.jsx
+++ version.jsx
@@ -1,5 +1,5 @@
 export default function getVersion() {
-  const version = "2.304.1 beta";
+  const version = "2.304.2 beta";
   const releaseDate = "25/06/2025";
   return { version, releaseDate };
 }
