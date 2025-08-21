f749c782aa3097e4554b323635192c87588b84ab
juniorArias02 <junior.arias02yt@gmail.com>
2025-08-21 16:19:10 -0500

    feat: se agreo nuevo modulo de pedido y equipos de computo

--- src/const/api.js
+++ src/const/api.js
@@ -3,5 +3,5 @@
 
 // export const URL_PATH = `http://${DOMINIO}:${PUERTO}/`;
 export const URL_PATH = 'https://clinicalhouse.co/deparSistemApi/';
-// export const URL_IMAGE = `${URL_PATH}/public/`;
-// export const URL_IMAGE2 = `${URL_PATH}/`;
+export const URL_IMAGE = `${URL_PATH}/public/`;
+export const URL_IMAGE2 = `${URL_PATH}/`;
