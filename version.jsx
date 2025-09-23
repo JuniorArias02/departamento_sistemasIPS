491b7ceff9623d3b6088d448b4485fae73f1d7e3
juniorArias02 <junior.arias02yt@gmail.com>
2025-09-23 08:32:03 -0500

    fix: cambio de fecha

--- version.jsx
+++ version.jsx
@@ -2,6 +2,6 @@ import packageJson from './package.json';
 
 export default function getVersion() {
   const version = packageJson.version;
-  const releaseDate = "15/09/2025"; 
+  const releaseDate = "23/09/2025"; 
   return { version, releaseDate };
 }
